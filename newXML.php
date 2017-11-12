2b8c4a6f67ca941e641dfcf2b94b291b8d0f4323
Srinidhi <emkay@wisc.edu>
2017-11-12 01:55:36 -0500

    l

--- newXML.php
+++ newXML.php
@@ -1,14 +1,15 @@
 <?php
 require("phpsqlajax_dbinfo.php");
-// Start XML file, create parent node
-$doc = domxml_new_doc("1.0");
-$node = $doc->create_element("markers");
-$parnode = $doc->append_child($node);
 
 $connection = new mysqli($server, $username, $password, $db);
 
 if (!$connection) {
   die('Not connected : ' . mysql_error());
 }
+// Start XML file, create parent node
+$doc = domxml_new_doc("1.0");
+$node = $doc->create_element("markers");
+$parnode = $doc->append_child($node);
+
 
 ?>
