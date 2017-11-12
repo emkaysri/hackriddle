2a12dd5c529d30fbf6079049cabbb06fa15eff7d
Srinidhi <emkay@wisc.edu>
2017-11-12 03:10:25 -0500

    hardcode af

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-session_start();
-    if (!isset($_SESSION["visits"]))
-        $_SESSION["visits"] = 0;
-    $_SESSION["visits"] = $_SESSION["visits"] + 1;
 header( 'Location: /index.html') ;
 ?>
