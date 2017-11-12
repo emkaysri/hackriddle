c2dfed050943832d7dba557c1c45cca7636754d6
Srinidhi <emkay@wisc.edu>
2017-11-11 22:09:32 -0500

    l

--- addValues.php
+++ addValues.php
@@ -10,7 +10,7 @@ if($_POST)
   $lat=$db->prepare($_POST['lat']);
   $lng=$db->prepare($_POST['lng']);
 // sql to create table
-$sql = "INSERT TABLE markers (found, lat, lng) VALUES ('0', '$lat', "$lng")";
+$sql = "INSERT TABLE markers (found, lat, lng) VALUES (0, $lat, $lng)";
 
 if ($connection->query($sql) === TRUE) {
     echo "added values";
