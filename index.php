40f53a8deb8b77a455f5928b3b3f3d6728a808f6
Srinidhi <emkay@wisc.edu>
2017-11-12 02:42:28 -0500

    hardcode

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-  echo file_get_contents("index.html");
+session_start();
+if(!$_SESSION['refresh']){
+  $_SESSION['refresh'] = -1;
+}
+$_SESSION['refresh']= 1 + $_SESSION['refresh'];
+header( 'Location: /index.html?', ) ;
 ?>
