76935feb49af69449fa16d834f957a8fa95be91c
Srinidhi <emkay@wisc.edu>
2017-11-11 20:44:26 -0500

    Restart

---
+++ createTable.php
@@ -0,0 +1,26 @@
+<?php
+require("phpsqlajax_dbinfo.php");
+$connection = mysqli_connect($dbhost, $username, $password, $dbname, $dbport);
+
+if (!$connection) {
+  die('Not connected : ' . mysql_error());
+}
+
+// sql to create table
+$sql = "CREATE TABLE markers (
+i  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `name` VARCHAR( 60 ) NOT NULL ,
+  `address` VARCHAR( 80 ) NOT NULL ,
+  `lat` FLOAT( 10, 6 ) NOT NULL ,
+  `lng` FLOAT( 10, 6 ) NOT NULL ,
+  `type` VARCHAR( 30 ) NOT NULL
+) ENGINE = MYISAM";
+
+if ($connection->query($sql) === TRUE) {
+    echo "Table markers created successfully";
+} else {
+    echo "Error creating table: " . $connection->error;
+}
+
+$connection->close();
+?>
---
+++ phpsqlajax_dbinfo.php
@@ -0,0 +1,10 @@
+<?php
+$dbhost = $_SERVER['dbmain.cc6bx3agn2kn.us-west-2.rds.amazonaws.com'];
+$dbport = $_SERVER['3306'];
+$dbname = $_SERVER['dbmain'];
+$charset = 'utf8' ;
+
+$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+$username = $_SERVER['srinidhi'];
+$password = $_SERVER['Sred1#ders'];
+?>
