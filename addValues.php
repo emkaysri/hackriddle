f0d4efd2ed1aaaab6a375b38af35e696d83a8a3e
Srinidhi <emkay@wisc.edu>
2017-11-12 00:37:48 -0500

    ls

--- addValues.php
+++ addValues.php
@@ -5,18 +5,21 @@ $connection = new mysqli($server, $username, $password, $db);
 if (!$connection) {
   die('Not connected : ' . mysql_error());
 }
-if($_POST)
+if($_PUT)
 {
-  $lat=$db->prepare($_POST['lat']);
-  $lng=$db->prepare($_POST['lng']);
+  $lat=$db->prepare($_PUT['lat']);
+  $lng=$db->prepare($_PUT['lng']);
 // sql to create table
 $sql = "INSERT TABLE markers (found, lat, lng) VALUES (0, $lat, $lng)";
+
 
 if ($connection->query($sql) === TRUE) {
     echo "added values";
 } else {
     echo "Error adding values " . $connection->error;
 }
-
+}else{
+  echo "no Post"
+}
 $connection->close();
 ?>
