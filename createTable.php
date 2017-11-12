8dbc90a66792a4007bdc1230e2b8c9ea1bad44a7
Srinidhi <emkay@wisc.edu>
2017-11-11 21:29:50 -0500

    d

--- createTable.php
+++ createTable.php
@@ -6,20 +6,13 @@ if (!$connection) {
   die('Not connected : ' . mysql_error());
 }
 
-$sql = "DROP TABLE markers";
-
-if ($connection->query($sql) === TRUE) {
-    echo "Table dropped";
-} else {
-    echo "Error creating table: " . $connection->error;
-}
 
 // sql to create table
 $sql = "CREATE TABLE people_loc (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  `foun` BIT NOT NULL,
-  `lat` FLOAT(10, 6) NOT NULL,
-  `lng` FLOAT(10, 6) NOT NULL
+  'id' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  'found' BIT NOT NULL,
+  'lat' FLOAT(10, 6) NOT NULL,
+  'lng' FLOAT(10, 6) NOT NULL
 )";
 
 if ($connection->query($sql) === TRUE) {
