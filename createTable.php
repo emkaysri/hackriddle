c869712c330c1698e3926ca07eb17310382e6b7c
Srinidhi <emkay@wisc.edu>
2017-11-11 21:19:24 -0500

    is table made?

--- createTable.php
+++ createTable.php
@@ -22,5 +22,20 @@ if ($connection->query($sql) === TRUE) {
     echo "Error creating table: " . $connection->error;
 }
 
+$sql = "SHOW TABLES FROM $dbname";
+$result = mysql_query($sql);
+
+if (!$result) {
+    echo "DB Error, could not list tables\n";
+    echo 'MySQL Error: ' . mysql_error();
+    exit;
+}
+
+while ($row = mysql_fetch_row($result)) {
+    echo "Table: {$row[0]}\n";
+}
+
+mysql_free_result($result);
+
 $connection->close();
 ?>
