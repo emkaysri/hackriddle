a8904f8c0dd898f244055a7060b7b3a672a9cab3
Srinidhi <emkay@wisc.edu>
2017-11-11 22:07:54 -0500

    a

--- createTable.php
+++ createTable.php
@@ -9,10 +9,10 @@ if (!$connection) {
 
 // sql to create table
 $sql = "CREATE TABLE people_loc (
-  'id' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  'found' CHAR(1) NOT NULL,
-  'lat' FLOAT(10, 6) NOT NULL,
-  'lng' FLOAT(10, 6) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  found CHAR(1) NOT NULL,
+  lat FLOAT(10, 6) NOT NULL,
+  lng FLOAT(10, 6) NOT NULL
 )";
 
 if ($connection->query($sql) === TRUE) {
