1ca0ae26af65915ace7fb3c71dc4e78a92c1559f
Srinidhi <emkay@wisc.edu>
2017-11-12 02:54:35 -0500

    hi

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ session_start();
     if (!isset($_SESSION["visits"]))
         $_SESSION["visits"] = 0;
     $_SESSION["visits"] = $_SESSION["visits"] + 1;
-header( 'Location: /index.html?', ) ;
+header( 'Location: /index.html') ;
 ?>
