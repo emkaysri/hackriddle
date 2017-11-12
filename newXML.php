6e2b87b040f45d2ea4054ae753eb8786ee6412dd
Srinidhi <emkay@wisc.edu>
2017-11-12 01:35:06 -0500

    test

--- newXML.php
+++ newXML.php
@@ -6,23 +6,21 @@ $doc = domxml_new_doc("1.0");
 $node = $doc->create_element("markers");
 $parnode = $doc->append_child($node);
 
-$connection = mysql_connect($server, $username, $password, $db);
+$connection = new mysqli($server, $username, $password, $db);
 
-if (!$connection) {
-  die('Not connected : ' . mysql_error());
-}
+
 
 // Select all the rows in the markers table
 $query = "SELECT * FROM people_loc WHERE found = '0'";
-$result = mysql_query($query);
-if (!$result) {
-  die('Invalid query: ' . mysql_error());
+$result = $connection->query($sql)
+if ($result->num_rows > 0) {
+  die('Invalid query:';
 }
 
 header("Content-type: text/xml");
 
 // Iterate through the rows, adding XML nodes for each
-while ($row = @mysql_fetch_assoc($result)){
+while ($row = $result->fetch_assoc()){
   // Add to XML document node
   $node = $doc->create_element("marker");
   $newnode = $parnode->append_child($node);
