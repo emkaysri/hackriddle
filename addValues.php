c126f1aab0fa3fd5e59cb7e39d08080848b5f49b
Srinidhi <emkay@wisc.edu>
2017-11-12 01:20:23 -0500

    fu

--- addValues.php
+++ addValues.php
@@ -8,7 +8,7 @@ if (!$connection) {
 $lat=$_GET['lat'];
 $lng=$_GET['lng'];
 // sql to create table
-$sql = "INSERT INTO TABLE people_loc (found, lat, lng) VALUES (0, $lat, $lng)";
+$sql = "INSERT INTO people_loc (found, lat, lng) VALUES (0, $lat, $lng)";
 
 
 if ($connection->query($sql) === TRUE) {
